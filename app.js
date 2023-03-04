3e42207fa5d127e285058ffdd5df9d6eebe4580c
Platypus88 <102930924+BipedPotato@users.noreply.github.com>
2023-03-04 14:33:32 -0600

    rlly big canvas will change later

---
+++ app.js
@@ -0,0 +1,29 @@
+var canvas = document.getElementById('canvas');
+canvas.style.position = 'fixed';
+var ctx = canvas.getContext('2d');
+var pos = { x: 0, y: 0 };
+document.addEventListener('mousemove', draw);
+document.addEventListener('mousedown', setPosition);
+document.addEventListener('mouseenter', setPosition);
+
+function setPosition(e) {
+  pos.x = e.clientX- canvas.offsetLeft;
+  pos.y = e.clientY- canvas.offsetTop;
+}
+
+
+function draw(e) {
+  if (e.buttons !== 1) return;
+
+  ctx.beginPath();
+
+  ctx.lineWidth = 5;
+  ctx.lineCap = 'round';
+  //ctx.strokeStyle = '#ffffff;
+
+  ctx.moveTo(pos.x, pos.y);
+  setPosition(e);
+  ctx.lineTo(pos.x, pos.y);
+
+  ctx.stroke(); 
+}
