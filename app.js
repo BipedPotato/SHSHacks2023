0a73083b0fbd6ba9c620b42f7bdd963dad6dd7e2
Platypus88 <102930924+BipedPotato@users.noreply.github.com>
2023-03-04 16:46:30 -0600

    Circle slider on some devices

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 var canvas = document.getElementById('drawingcanvas');
 var ctx = canvas.getContext('2d');
 var pos = { x: 0, y: 0 };
-document.addEventListener('mousemove', draw);
-document.addEventListener('mousedown', setPosition);
-document.addEventListener('mouseenter', setPosition);
+canvas.addEventListener('mousemove', draw);
+canvas.addEventListener('mousedown', setPosition);
+canvas.addEventListener('mouseenter', setPosition);
 
 function setPosition(e) {
   let scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
@@ -14,21 +14,18 @@ function setPosition(e) {
 
 
 function draw(e) {
-  if(document.activeElement == canvas)
-  {
-    if (e.buttons !== 1) return;
+  if (e.buttons !== 1) return;
 
-    ctx.beginPath();
-  
-    ctx.lineWidth = 5;
-    ctx.lineCap = 'round';
-    //ctx.strokeStyle = '#ffffff;
-  
-    ctx.moveTo(pos.x, pos.y);
-    setPosition(e);
-    ctx.lineTo(pos.x, pos.y);
-  
-    ctx.stroke(); 
-  }
+  ctx.beginPath();
+
+  ctx.lineWidth = 5;
+  ctx.lineCap = 'round';
+  //ctx.strokeStyle = '#ffffff;
+
+  ctx.moveTo(pos.x, pos.y);
+  setPosition(e);
+  ctx.lineTo(pos.x, pos.y);
+
+  ctx.stroke(); 
 
 }
