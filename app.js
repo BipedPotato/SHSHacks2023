00b9cd707d0441c57fa9f24ee9a2205cfe4850d3
Platypus88 <102930924+BipedPotato@users.noreply.github.com>
2023-03-04 23:36:50 -0600

    percentage orientation

--- app.js
+++ app.js
@@ -3,91 +3,121 @@ document.body.appendChild(output);
 
 var oldLog = console.log;
 
-console.log = function( ...items ) {
+console.log = function (...items) {
 
-    oldLog.apply(this,items);
+  oldLog.apply(this, items);
 
-    items.forEach( (item,i)=>{
-        items[i] = (typeof item === 'object' ? JSON.stringify(item,null,4) : item);
-    });
-    output.innerHTML += items.join(' ') + '<br />';
+  items.forEach((item, i) => {
+    items[i] = (typeof item === 'object' ? JSON.stringify(item, null, 4) : item);
+  });
+  output.innerHTML += items.join(' ') + '<br />';
 
 };
 
-function consoleInput( data ) {
-    console.log( data + '<br />' );
-    try {
-        console.log( eval( data ) );
-    } catch (e) {
-        console.log( e.stack );
-    }
+function consoleInput(data) {
+  console.log(data + '<br />');
+  try {
+    console.log(eval(data));
+  } catch (e) {
+    console.log(e.stack);
+  }
 }
 //console.log("plz send help");
 var canvas = document.getElementById('drawingcanvas');
 var ctx = canvas.getContext('2d');
-var pos = { x: 0, y: 0 };
-canvas.addEventListener('mousemove', draw);
-canvas.addEventListener('mousedown', setPosition);
+canvas.width = window.innerWidth / 2;
+canvas.height = window.innerHeight / 3;
 
+var drawing = false;
+var mousePos = { x: 0, y: 0 };
+var lastPos = mousePos;
+canvas.addEventListener("mousedown", function (e) {
+  drawing = true;
+  lastPos = getMousePos(canvas, e);
+}, false);
+canvas.addEventListener("mouseup", function (e) {
+  drawing = false;
+}, false);
+canvas.addEventListener("mousemove", function (e) {
+  mousePos = getMousePos(canvas, e);
+}, false);
 
+function getMousePos(canvasDom, mouseEvent) {
+  var rect = canvas.getBoundingClientRect(),
+    scaleX = canvas.width / rect.width,
+    scaleY = canvas.height / rect.height;
+
+  return {
+    x: (mouseEvent.clientX - rect.left) * scaleX,
+    y: (mouseEvent.clientY - rect.top) * scaleY
+  }
+}
+window.requestAnimFrame = (function (callback) {
+  return window.requestAnimationFrame ||
+    window.webkitRequestAnimationFrame ||
+    window.mozRequestAnimationFrame ||
+    window.oRequestAnimationFrame ||
+    window.msRequestAnimaitonFrame ||
+    function (callback) {
+      window.setTimeout(callback, 1000 / 60);
+    };
+})();
+
+function renderCanvas() {
+  if (drawing) {
+    
+    ctx.moveTo(lastPos.x, lastPos.y);
+    ctx.lineTo(mousePos.x, mousePos.y);
+    ctx.stroke();
+    lastPos = mousePos;
+  }
+}
+
+(function drawLoop() {
+  requestAnimFrame(drawLoop);
+  renderCanvas();
+})();
 canvas.addEventListener("touchstart", function (e) {
   mousePos = getTouchPos(canvas, e);
   var touch = e.touches[0];
   var mouseEvent = new MouseEvent("mousedown", {
-  clientX: touch.clientX,
-  clientY: touch.clientY
+    clientX: touch.clientX,
+    clientY: touch.clientY
   });
   canvas.dispatchEvent(mouseEvent);
-});
-
+}, false);
 canvas.addEventListener("touchend", function (e) {
   var mouseEvent = new MouseEvent("mouseup", {});
   canvas.dispatchEvent(mouseEvent);
-});
-
+}, false);
 canvas.addEventListener("touchmove", function (e) {
   var touch = e.touches[0];
   var mouseEvent = new MouseEvent("mousemove", {
-  clientX: touch.clientX,
-  clientY: touch.clientY
+    clientX: touch.clientX,
+    clientY: touch.clientY
   });
   canvas.dispatchEvent(mouseEvent);
-});
+}, false);
 
-// Get the position of a touch relative to the canvas
 function getTouchPos(canvasDom, touchEvent) {
   var rect = canvasDom.getBoundingClientRect();
   return {
-  x: touchEvent.touches[0].clientX - rect.left,
-  y: touchEvent.touches[0].clientY - rect.top
+    x: touchEvent.touches[0].clientX - rect.left,
+    y: touchEvent.touches[0].clientY - rect.top
   };
 }
-
-
-
-
-function setPosition(e) {
-  console.log(pos.x+","+pos.y);
-  let scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
-    scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-  pos.x = e.clientX - canvas.offsetLeft + scrollLeft;
-  pos.y = e.clientY - canvas.offsetTop + scrollTop;
-}
-
-
-function draw(e) {
-  if (e.buttons !== 1) return;
-
-  ctx.beginPath();
-
-  ctx.lineWidth = document.getElementById("drawingSize").value;
-  ctx.lineCap = 'round';
-  ctx.strokeStyle = document.getElementById("colorpicker").value;
-
-  ctx.moveTo(pos.x, pos.y);
-  setPosition(e);
-  ctx.lineTo(pos.x, pos.y);
-
-  ctx.stroke();
-
-}
+document.body.addEventListener("touchstart", function (e) {
+  if (e.target == canvas) {
+    e.preventDefault();
+  }
+}, false);
+document.body.addEventListener("touchend", function (e) {
+  if (e.target == canvas) {
+    e.preventDefault();
+  }
+}, false);
+document.body.addEventListener("touchmove", function (e) {
+  if (e.target == canvas) {
+    e.preventDefault();
+  }
+}, false);
