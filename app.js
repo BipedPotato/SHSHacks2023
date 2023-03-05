1b29925acca79ff903309f6ed41b362fd810e019
Platypus88 <102930924+BipedPotato@users.noreply.github.com>
2023-03-04 19:29:09 -0600

    another mobile attempt

--- app.js
+++ app.js
@@ -5,9 +5,41 @@ canvas.addEventListener('mousemove', draw);
 canvas.addEventListener('mousedown', setPosition);
 canvas.addEventListener('mouseenter', setPosition);
 
-canvas.addEventListener('touchmove', drawMobile);
-canvas.addEventListener('touchdown', setPosition);
-canvas.addEventListener('touchstart', setPosition);
+
+// Set up touch events for mobile, etc
+canvas.addEventListener("touchstart", function (e) {
+  mousePos = getTouchPos(canvas, e);
+  var touch = e.touches[0];
+  var mouseEvent = new MouseEvent("mousedown", {
+  clientX: touch.clientX,
+  clientY: touch.clientY
+  });
+  canvas.dispatchEvent(mouseEvent);
+}, false);
+canvas.addEventListener("touchend", function (e) {
+  var mouseEvent = new MouseEvent("mouseup", {});
+  canvas.dispatchEvent(mouseEvent);
+}, false);
+canvas.addEventListener("touchmove", function (e) {
+  var touch = e.touches[0];
+  var mouseEvent = new MouseEvent("mousemove", {
+  clientX: touch.clientX,
+  clientY: touch.clientY
+  });
+  canvas.dispatchEvent(mouseEvent);
+}, false);
+
+// Get the position of a touch relative to the canvas
+function getTouchPos(canvasDom, touchEvent) {
+  var rect = canvasDom.getBoundingClientRect();
+  return {
+  x: touchEvent.touches[0].clientX - rect.left,
+  y: touchEvent.touches[0].clientY - rect.top
+  };
+}
+
+
+
 
 function setPosition(e) {
   let scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
@@ -17,6 +49,7 @@ function setPosition(e) {
 }
 
 function setPositionMobile(e) {
+  //document.getElementById("drawingSize").value = 100;
   let scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
     scrollTop = window.pageYOffset || document.documentElement.scrollTop;
   pos.x = e.touches[0].clientX - canvas.offsetLeft + scrollLeft;
